3740be640243b747251dc8bb31cd4b48dad162dc
andr <andrey.lischina@gmail.com>
2015-04-05 00:03:41 +0300

    .

--- test.cmd
+++
@@ -1,3 +0,0 @@
-istanbul cover ^
-    "%USERPROFILE%\AppData\Roaming\npm\node_modules\mocha\bin\_mocha" ^
-    -- -R spec --compilers coffee:coffee-script/register
--- watch.cmd
+++
@@ -1,3 +0,0 @@
-mocha --watch --compilers ^
-coffee:coffee-script/register ^
--R spec
